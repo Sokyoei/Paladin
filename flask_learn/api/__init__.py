fc5c873a4eb013ae354020dcfb89514d9b5f8af6
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2025-12-03 15:54:49 +0800

    update flask_learn

--- flask_learn/api/__init__.py
+++ flask_learn/api/__init__.py
@@ -1,5 +1,10 @@
+from .music import bp as music_bp
 from .user import bp as user_bp
 
-all_blueprints = [user_bp]
+all_blueprints = [
+    # add your blueprints here
+    user_bp,
+    music_bp,
+]
 
 __all__ = ['all_blueprints']
---
+++ flask_learn/api/music.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+bp = Blueprint("music", __name__, url_prefix="")
