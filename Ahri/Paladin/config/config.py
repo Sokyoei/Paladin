4a1ba7e5ea97401197db9ca7487930d5169b377c
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2025-12-19 17:07:23 +0800

    update minio_manager and config

--- Ahri/Paladin/config/config.py
+++ Ahri/Paladin/config/config.py
@@ -1,6 +1,11 @@
+"""
+config form envioronment variables and .env file
+"""
+
 from functools import lru_cache
 from pathlib import Path
 
+from pydantic import MySQLDsn, PostgresDsn, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from Ahri.Paladin import PALADIN_ROOT
@@ -9,8 +14,53 @@ from Ahri.Paladin import PALADIN_ROOT
 class Settings(BaseSettings):
     DEBUG: bool = False
 
+    # dir
     LOG_DIR: Path = PALADIN_ROOT / "logs"
     DOWNLOAD_DIR: Path = PALADIN_ROOT / "downloads"
+
+    # PostgreSQL
+    POSTGRESQL_HOST: str = "127.0.0.1"
+    POSTGRESQL_PORT: int = 5432
+    POSTGRESQL_USERNAME: str = "user"
+    POSTGRESQL_PASSWORD: str = "password"
+    POSTGRESQL_DB: str = "database"
+
+    @computed_field
+    @property
+    def SQLALCHEMY_DATABASE_URI_POSTGRESQL(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            username=self.POSTGRESQL_USERNAME,
+            password=self.POSTGRESQL_PASSWORD,
+            host=self.POSTGRESQL_HOST,
+            port=self.POSTGRESQL_PORT,
+            path=self.POSTGRESQL_DB,
+        )
+
+    # MySQL
+    MYSQL_HOST: str = "127.0.0.1"
+    MYSQL_PORT: int = 3306
+    MYSQL_USERNAME: str = "user"
+    MYSQL_PASSWORD: str = "password"
+    MYSQL_DB: str = "database"
+
+    @computed_field
+    @property
+    def SQLALCHEMY_DATABASE_URI_MYSQL(self) -> MySQLDsn:
+        return MySQLDsn.build(
+            scheme="mysql+aiomysql",
+            username=self.MYSQL_USERNAME,
+            password=self.MYSQL_PASSWORD,
+            host=self.MYSQL_HOST,
+            port=self.MYSQL_PORT,
+            path=self.MYSQL_DB,
+        )
+
+    # Minio
+    MINIO_BUCKET_NAME: str = "paladin"
+    MINIO_ENDPOINT: str = "127.0.0.1:9000"
+    MINIO_ACCESS_KEY: str = "minioadmin"
+    MINIO_SECRET_KEY: str = "minioadmin"
 
     model_config = SettingsConfigDict(
         env_file=[PALADIN_ROOT / ".env", PALADIN_ROOT / ".env.dev", PALADIN_ROOT / ".env.prod"],
@@ -26,5 +76,16 @@ def get_settings() -> Settings:
 settings = get_settings()
 
 DEBUG = settings.DEBUG
+
+# dir
 LOG_DIR = settings.LOG_DIR
 DOWNLOAD_DIR = settings.DOWNLOAD_DIR
+
+# Minio
+MINIO_BUCKET_NAME = settings.MINIO_BUCKET_NAME
+MINIO_ENDPOINT = settings.MINIO_ENDPOINT
+MINIO_ACCESS_KEY = settings.MINIO_ACCESS_KEY
+MINIO_SECRET_KEY = settings.MINIO_SECRET_KEY
+
+# SQLAlchemy URI
+SQLALCHEMY_DATABASE_URI = str(settings.SQLALCHEMY_DATABASE_URI_POSTGRESQL)
--- Ahri/Paladin/manager/minio_manager.py
+++ Ahri/Paladin/manager/minio_manager.py
@@ -1,114 +1,131 @@
 import asyncio
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
-from typing import Dict, List, Optional
 
 from loguru import logger
 from minio import Minio
 from minio.error import S3Error
 
-from Ahri.Paladin.config.config import MINIO_ACCESS_KEY, MINIO_ENDPOINT, MINIO_SECRET_KEY
+from Ahri.Paladin.config.config import MINIO_ACCESS_KEY, MINIO_BUCKET_NAME, MINIO_ENDPOINT, MINIO_SECRET_KEY
 
 
 class MinioManager(object):
 
-    def __init__(self, endpoint: str, access_key: str, secret_key: str, secure: bool = True):
+    def __init__(
+        self, endpoint: str, access_key: str, secret_key: str, bucket_name=MINIO_BUCKET_NAME, secure: bool = True
+    ):
         self.client = Minio(endpoint, access_key=access_key, secret_key=secret_key, secure=secure)
-        self.http_prefix = "https://" if secure else "http://"
+        self.bucket_name = bucket_name
 
-    async def list_objects(self, bucket_name: str, prefix: str = "", filter=[]) -> List:
+        if not self.client.bucket_exists(self.bucket_name):
+            self.client.make_bucket(self.bucket_name)
 
-        def _list_objects():
-            try:
-                objects = []
-                for obj in self.client.list_objects(bucket_name=bucket_name, prefix=prefix, recursive=True):
-                    if not obj.object_name.endswith("/") and obj.object_name.lower().endswith(filter):
-                        objects.append(obj)
-                logger.info(f"Bucket {bucket_name} contains {len(objects)} files")
-                return objects
-            except S3Error as e:
-                logger.error(f"Failed to get minio object list `{bucket_name}`: {e}")
-                return []
-            except Exception as e:
-                logger.error(f"Unknown error when getting file list `{bucket_name}`: {e}")
-                return []
+    def list_objects(self, prefix: str = "", filter=[]) -> list:
+        try:
+            objects = []
+            for obj in self.client.list_objects(bucket_name=self.bucket_name, prefix=prefix, recursive=True):
+                if not obj.object_name.endswith("/") and obj.object_name.lower().endswith(filter):
+                    objects.append(obj)
+            logger.info(f"Bucket {self.bucket_name} contains {len(objects)} files")
+            return objects
+        except S3Error as e:
+            logger.error(f"Failed to get minio object list `{self.bucket_name}`: {e}")
+            return []
+        except Exception as e:
+            logger.error(f"Unknown error when getting file list `{self.bucket_name}`: {e}")
+            return []
 
-        return await asyncio.to_thread(_list_objects)
+    async def list_objects_async(self, prefix: str = "", filter=[]) -> list:
+        return await asyncio.to_thread(self.list_objects, prefix, filter)
 
-    async def download_file(self, bucket_name: str, object_name: str, file_path: Path) -> Optional[Path]:
-        def _download_file() -> Optional[Path]:
-            try:
-                file_path.parent.mkdir(parents=True, exist_ok=True)
+    def download_file(self, object_name: str, file_path: Path) -> Path | None:
+        try:
+            file_path.parent.mkdir(parents=True, exist_ok=True)
 
-                if file_path.exists():
-                    logger.debug(f"File already exists: {file_path}")
-                    return file_path
-
-                self.client.fget_object(bucket_name=bucket_name, object_name=object_name, file_path=str(file_path))
-                logger.info(f"File downloaded, minio: `{bucket_name}/{object_name}` -> local: `{file_path}`")
+            if file_path.exists():
+                logger.debug(f"File already exists: {file_path}")
                 return file_path
 
-            except S3Error as e:
-                logger.error(f"File download `{bucket_name}/{object_name}` minio error: {e}")
+            self.client.fget_object(bucket_name=self.bucket_name, object_name=object_name, file_path=str(file_path))
+            logger.info(f"File downloaded, minio: `{self.bucket_name}/{object_name}` -> local: `{file_path}`")
+            return file_path
+
+        except S3Error as e:
+            logger.error(f"File download `{self.bucket_name}/{object_name}` minio error: {e}")
+            return None
+        except Exception as e:
+            logger.error(f"File download `{self.bucket_name}/{object_name}` unknown error: {e}")
+            return None
+
+    async def download_file_async(self, object_name: str, file_path: Path) -> Path | None:
+        return await asyncio.to_thread(self.download_file, object_name, file_path)
+
+    def upload_file(self, file_path: Path, object_name: str | None = None):
+        try:
+            if not file_path.exists():
+                logger.error(f"File not found: {file_path}")
                 return None
-            except Exception as e:
-                logger.error(f"File download `{bucket_name}/{object_name}` unknown error: {e}")
-                return None
 
-        return await asyncio.to_thread(_download_file)
+            final_object_name = object_name or file_path.name
 
-    async def upload_file(self, bucket_name: str, file_path: Path, object_name: Optional[str] = None):
-        def _upload_file() -> bool:
-            try:
-                if not file_path.exists():
-                    logger.error(f"File not found: {file_path}")
-                    return None
+            self.client.fput_object(
+                bucket_name=self.bucket_name, object_name=final_object_name, file_path=str(file_path)
+            )
+            minio_url = self.client.presigned_get_object(bucket_name=self.bucket_name, object_name=final_object_name)
 
-                final_object_name = object_name or file_path.name
+            logger.info(f"File uploaded, local: {file_path} -> minio: {minio_url}")
+            return minio_url
+        except S3Error as e:
+            logger.error(f"File upload `{self.bucket_name}/{object_name}` minio error: {e}")
+            return None
+        except Exception as e:
+            logger.error(f"File upload `{self.bucket_name}/{object_name}` unknown error: {e}")
+            return None
 
-                if not self.client.bucket_exists(bucket_name):
-                    self.client.make_bucket(bucket_name)
+    async def upload_file_async(self, file_path: Path, object_name: str | None = None):
+        return await asyncio.to_thread(self.upload_file, file_path, object_name)
 
-                self.client.fput_object(
-                    bucket_name=bucket_name, object_name=final_object_name, file_path=str(file_path)
+    def upload_files(self, file_paths_and_object_names: dict[Path, str]):
+        upload_file_urls = []
+
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            futures = [
+                executor.submit(
+                    self.upload_file, bucket_name=self.bucket_name, file_path=file_path, object_name=object_name
                 )
-                minio_path = f"{self.http_prefix}{MINIO_ENDPOINT}/{bucket_name}/{final_object_name}"
+                for file_path, object_name in file_paths_and_object_names.items()
+            ]
 
-                logger.info(f"File uploaded, local: {file_path} -> minio: {minio_path}")
-                return minio_path
-            except S3Error as e:
-                logger.error(f"File upload `{bucket_name}/{object_name}` minio error: {e}")
-                return None
-            except Exception as e:
-                logger.error(f"File upload `{bucket_name}/{object_name}` unknown error: {e}")
-                return None
+            for future in as_completed(futures):
+                upload_file_urls.append(future.result())
 
-        return await asyncio.to_thread(_upload_file)
+        return upload_file_urls
 
-    async def upload_files(self, bucket_name: str, file_paths_and_object_names: Dict[Path, str]):
+    async def upload_files_async(self, file_paths_and_object_names: dict[Path, str]):
         upload_tasks = []
 
         for file_path, object_name in file_paths_and_object_names.items():
-            upload_task = self.upload_file(bucket_name=bucket_name, file_path=file_path, object_name=object_name)
+            upload_task = self.upload_file(bucket_name=self.bucket_name, file_path=file_path, object_name=object_name)
             upload_tasks.append(upload_task)
         results = await asyncio.gather(*upload_tasks)
 
-        successful_uploads = [result for result in results if result is not None]
-        return successful_uploads
+        upload_file_urls = [result for result in results if result is not None]
+        return upload_file_urls
 
-    async def delete_file(self, bucket_name: str, object_name: str) -> bool:
-        def _delete_file() -> bool:
-            try:
-                self.client.remove_object(bucket_name=bucket_name, object_name=object_name)
-                logger.info(f"File deleted, minio: `{bucket_name}/{object_name}`")
-                return True
-            except S3Error as e:
-                logger.error(f"File delete `{bucket_name}/{object_name}` minio error: {e}")
-                return False
-            except Exception as e:
-                logger.error(f"File delete `{bucket_name}/{object_name}` unknown error: {e}")
-                return False
+    def delete_file(self, object_name: str) -> bool:
+        try:
+            self.client.remove_object(bucket_name=self.bucket_name, object_name=object_name)
+            logger.info(f"File deleted, minio: `{self.bucket_name}/{object_name}`")
+            return True
+        except S3Error as e:
+            logger.error(f"File delete `{self.bucket_name}/{object_name}` minio error: {e}")
+            return False
+        except Exception as e:
+            logger.error(f"File delete `{self.bucket_name}/{object_name}` unknown error: {e}")
+            return False
 
-        return await asyncio.to_thread(_delete_file)
+    async def delete_file_async(self, object_name: str) -> bool:
+        return await asyncio.to_thread(self.delete_file, object_name)
 
 
 minio_manager = MinioManager(
