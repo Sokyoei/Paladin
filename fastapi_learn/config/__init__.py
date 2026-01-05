432d556f8c51d6249f4e9d9a7e77c5ed8b50ca1d
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2026-01-05 15:41:18 +0800

    fix fastapi_learn.config

--- fastapi_learn/config/__init__.py
+++ fastapi_learn/config/__init__.py
@@ -1,13 +1,12 @@
 from .admin_manager import admin_manager
 from .config import settings
 from .database import db_instance, get_db, get_manual_db
-from .redis_config import AsyncRedisConfig, RedisConfig
+from .redis_manager import aioredis_manager
 from .websocket_manager import websocket_manager
 
 __all__ = [
-    "AsyncRedisConfig",
-    "RedisConfig",
     "admin_manager",
+    "aioredis_manager",
     "db_instance",
     "get_db",
     "get_manual_db",
